9d8a39dedcb948348898303a45cac8d440a6558c
Isabela Blucher <isabelablucher@gmail.com>
2019-04-13 19:26:24 -0300

    Generate classes from distributions

--- EP3/ep3.py
+++ EP3/ep3.py
@@ -1,5 +1,6 @@
 import numpy as np
 import random
+import matplotlib.pyplot as plt
 
 def logistic_fit(X, y, w = None, batch_size = None, learning_rate = 1e-2, num_iterations = 1000, return_history = False):
     '''
@@ -26,13 +27,9 @@ def logistic_fit(X, y, w = None, batch_size = None, learning_rate = 1e-2, num_it
 
     X = np.concatenate((np.ones((N, 1)), X), axis=1)
 
-    print(X.shape)
-    print(w.shape)
-
     while it <= num_iterations:
         z = np.dot(X, w)
         h = 1.0 / (1 + np.exp(-z))
-        print(y.shape)
         gradient = np.dot(X.T, (h - y)) / N 
         w = w - learning_rate * gradient
         it += 1
@@ -48,19 +45,21 @@ def logistic_predict(X, w):
     :param w: array 1D (d + 1 x 1) correspondendo a um vetor de pesos incial
     :return predictions: array 1D (N x 1) de predições
     '''
+    X = np.concatenate((np.ones((X.shape[0], 1)), X), axis=1)
     
     z = np.dot(X, w)
     predictions = 1.0 / (1 + np.exp(-z))
-
-
+    
     return predictions
 
 def plot_2D(mean1, mean2, cov1, cov2, size1, size2):
-    x1, y1 = np.random.multivariate_normal(mean1, cov1, size1).T
-    x2, y2 = np.random.multivariate_normal(mean2, cov2, size2).T
+    x1 = np.random.multivariate_normal(mean1, cov1, size1).T
+    y1 = np.ones((x1.shape[1], 1))
+    x2 = np.random.multivariate_normal(mean2, cov2, size2).T
+    y2 = np.negative(y1)
 
     X = np.concatenate((x1.T, x2.T), axis = 0)
-    y = np.concatenate((y1.T, y2.T), axis = 0)
+    y = np.concatenate((y1, y2), axis = 0)
 
     return (X, y)
 
@@ -68,15 +67,23 @@ mean1 = (4, 2)
 mean2 = (10, 2)
 cov1 = [[2, 0], [0, 2]]
 
-X, y = plot_2D(mean1, mean2, cov1, cov1, 10000, 10000)
+X, y = plot_2D(mean1, mean2, cov1, cov1, 10, 10)
 
-# size = len(X)
-# y1 = np.ones((size, 1))
-# y2 = [-1 for _ in range(size)]
-# y2 = np.reshape(y2, (size, 1))
+print(X)
+print(y)
 
-# y = np.concatenate((y1, y2), axis = 0)
+w = logistic_fit(X, y)
+pred = logistic_predict(X, w)
 
-w = logistic_fit(np.reshape(X, (X.shape[0], 1)), np.reshape(y, (y.shape[0], 1)))
+print(w, pred)
 
-# pred = logistic_predict(X, w)
+# plt.plot(data1[0], data1[1], 'x', marker = '.')
+# plt.plot(data2[0], data2[1], 'x', marker = '.')
+
+# plt.axis('equal')
+# plt.show()
+
+
+
+
+
