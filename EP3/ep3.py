d82dee9a8b62caf022c1f64e15f179d366d10aaf
Isabela Blucher <isabelablucher@gmail.com>
2019-04-13 18:56:49 -0300

    Changing dimensions of arrays

--- EP3/ep3.py
+++ EP3/ep3.py
@@ -16,19 +16,23 @@ def logistic_fit(X, y, w = None, batch_size = None, learning_rate = 1e-2, num_it
     '''
 
     it = 0
-    N = X.shape[0]
+    N, d = X.shape
     
     if w == None:
         #pass
         w = []
-        w = [np.random.uniform(0.1, 5) for _ in range(N)]
-        w = np.reshape(w, (1, N))
+        w = [np.random.uniform(0.1, 5) for _ in range(d + 1)]
+        w = np.reshape(w, (d + 1, 1))
 
     X = np.concatenate((np.ones((N, 1)), X), axis=1)
+
+    print(X.shape)
+    print(w.shape)
 
     while it <= num_iterations:
         z = np.dot(X, w)
         h = 1.0 / (1 + np.exp(-z))
+        print(y.shape)
         gradient = np.dot(X.T, (h - y)) / N 
         w = w - learning_rate * gradient
         it += 1
@@ -52,35 +56,27 @@ def logistic_predict(X, w):
     return predictions
 
 def plot_2D(mean1, mean2, cov1, cov2, size1, size2):
-    data1 = np.random.multivariate_normal(mean1, cov1, size1).T
-    data2 = np.random.multivariate_normal(mean2, cov2, size2).T
+    x1, y1 = np.random.multivariate_normal(mean1, cov1, size1).T
+    x2, y2 = np.random.multivariate_normal(mean2, cov2, size2).T
 
-    X = np.concatenate((data1.T, data2.T), axis = 0)
+    X = np.concatenate((x1.T, x2.T), axis = 0)
+    y = np.concatenate((y1.T, y2.T), axis = 0)
 
-    return (X)
+    return (X, y)
 
 mean1 = (4, 2)
 mean2 = (10, 2)
 cov1 = [[2, 0], [0, 2]]
 
-X = plot_2D(mean1, mean2, cov1, cov1, 10000, 10000)
+X, y = plot_2D(mean1, mean2, cov1, cov1, 10000, 10000)
 
-size = len(X)
-y1 = np.ones((size, 1))
-y2 = [-1 for _ in range(size)]
-y2 = np.reshape(y2, (size, 1))
+# size = len(X)
+# y1 = np.ones((size, 1))
+# y2 = [-1 for _ in range(size)]
+# y2 = np.reshape(y2, (size, 1))
 
-y = np.concatenate((y1, y2), axis = 0)
+# y = np.concatenate((y1, y2), axis = 0)
 
-w = logistic_fit(X, y)
+w = logistic_fit(np.reshape(X, (X.shape[0], 1)), np.reshape(y, (y.shape[0], 1)))
 
-#pre = logistic_predict(X, w)
-
-
-
-
-"""
-c = np.ones(len(a[0]))
-
-w = logistic_fit(a, c)
-"""
+# pred = logistic_predict(X, w)
